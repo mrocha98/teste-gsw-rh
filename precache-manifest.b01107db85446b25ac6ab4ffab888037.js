c8b1e898e53f795b0803c70ca5ec16ed8a05ddc9
Matheus Rocha <matheus_rocha98@outlook.com>
2020-02-11 16:21:20 -0300

    Updates

---
+++ precache-manifest.b01107db85446b25ac6ab4ffab888037.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3363011b758d55fd333939a69b338bd",
+    "url": "/teste-gw-rh/index.html"
+  },
+  {
+    "revision": "6dad03899eb9edf4cf53",
+    "url": "/teste-gw-rh/static/css/2.3a0b6cad.chunk.css"
+  },
+  {
+    "revision": "c3cae0250b298e937f6a",
+    "url": "/teste-gw-rh/static/css/main.da559988.chunk.css"
+  },
+  {
+    "revision": "6dad03899eb9edf4cf53",
+    "url": "/teste-gw-rh/static/js/2.ef25fdc9.chunk.js"
+  },
+  {
+    "revision": "ec6abfcadd0dfdd54fcc46cfa40e2c72",
+    "url": "/teste-gw-rh/static/js/2.ef25fdc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3cae0250b298e937f6a",
+    "url": "/teste-gw-rh/static/js/main.3c8adac5.chunk.js"
+  },
+  {
+    "revision": "3125fbdcd13f52c39319",
+    "url": "/teste-gw-rh/static/js/runtime-main.8a0baf5e.js"
+  },
+  {
+    "revision": "cf93567ea0d823203444cd421a578e0c",
+    "url": "/teste-gw-rh/static/media/gsw-logo-flip.cf93567e.svg"
+  },
+  {
+    "revision": "92c34a6076c88513028ab251553ee6d5",
+    "url": "/teste-gw-rh/static/media/logo-gsw-minimal.92c34a60.svg"
+  },
+  {
+    "revision": "2c37ee097689912d51d4332774c1585d",
+    "url": "/teste-gw-rh/static/media/logo-gsw.2c37ee09.svg"
+  }
+]);
